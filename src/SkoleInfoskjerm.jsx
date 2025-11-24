610a881758bac5f17829887ebcbc369f2befd830
narcishamzic-web <narcis.hamzic@gmail.com>
2025-11-24 09:47:41 +0100

    Update SkoleInfoskjerm.jsx

--- src/SkoleInfoskjerm.jsx
+++ src/SkoleInfoskjerm.jsx
@@ -15,7 +15,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "./components/ui/dialog";
-import Input from "./components/ui/input";
+import { Input } from "./components/ui/input";
 import { Textarea } from "./components/ui/textarea";
 import { Switch } from "./components/ui/switch";
 import { Separator } from "./components/ui/separator";
